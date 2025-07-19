5cb96f7956ed1317a73b89877e07e4999367a4da
TSoutoDev <tsouto.dev@gmail.com>
2025-07-19 16:59:33 -0300

    Ajustes

--- TSmartClinic.API.GatewayOcelot/Program.cs
+++ TSmartClinic.API.GatewayOcelot/Program.cs
@@ -1,35 +1,32 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-using TSmartClinic.Core.Infra.CrossCutting.Extensions;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddJwtBearer(builder.Configuration);
-
-//builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Configuration
-    .SetBasePath(builder.Environment.ContentRootPath)
-    .AddOcelot($"{builder.Environment.ContentRootPath}/config/{builder.Environment.EnvironmentName}", builder.Environment)
-    .AddEnvironmentVariables();
-
-builder.Services.AddOcelot(builder.Configuration);
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.AllowAnyOrigin()
-                          .AllowAnyHeader()
-                          .AllowAnyMethod());
-});
+// Add services to the container.
 
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+// Configure the HTTP request pipeline.
 
-app.UseCors("AllowSpecificOrigin");
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-app.MapGet("/", () => "Hello World!");
-app.MapControllers();
-await app.UseOcelot();
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
 
 app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
