8b2c17801bf8354761f27520d819107677e9fdeb
TSoutoDev <tsouto.dev@gmail.com>
2025-07-27 22:13:54 -0300

    Ajuste de login

--- TSmartClinic.Presentation/Controllers/HomeController.cs
+++ TSmartClinic.Presentation/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using TSmartClinic.Presentation.Models;
 using TSmartClinic.Presentation.Settings;
 
 namespace TSmartClinic.Presentation.Controllers
@@ -15,7 +16,24 @@ namespace TSmartClinic.Presentation.Controllers
         {
             ViewBag.APIGateway = _urlApiSettings.Value.ApiGateway;
 
-            return View();
+            var model = new DashboardViewModel
+            {
+                TotalAcoes = 25,
+                AcoesAtivas = 18,
+                AcoesInativas = 7,
+                UltimasAcoes = new List<string>
+        {
+            "Inspeção de segurança - Almoxarifado",
+            "Capacitação em NR-10",
+            "Revisão de EPIs - Equipe A",
+            "Auditoria interna de SST",
+            "Campanha de prevenção contra acidentes"
+        },
+                Meses = new List<string> { "Fev", "Mar", "Abr", "Mai", "Jun", "Jul" },
+                AcoesPorMes = new List<int> { 2, 3, 4, 3, 5, 8 }
+            };
+            return View(model); // Vai para Views/Home/Index.cshtml
+   
         }
     }
 }
---
+++ TSmartClinic.Presentation/Models/DashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TSmartClinic.Presentation.Models
+{
+
+    public class DashboardViewModel
+    {
+        public int TotalAcoes { get; set; }
+        public int AcoesAtivas { get; set; }
+        public int AcoesInativas { get; set; }
+        public List<string> UltimasAcoes { get; set; }
+
+        // Estas são as propriedades que estavam faltando:
+        public List<int> AcoesPorMes { get; set; }      // Ex: [2, 3, 5, 4, 1]
+        public List<string> Meses { get; set; }         // Ex: ["Jan", "Fev", "Mar", ...]
+    }
+}
+
+
+
